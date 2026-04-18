34d5c5238056ed7fc90fec4dca2e2a7f83181190
Yanice <git@yanice.me>
2026-04-18 18:34:53 +0200

    u16 for size

--- bin/packets/utils/packet_reader.ml
+++ bin/packets/utils/packet_reader.ml
@@ -4,9 +4,9 @@ type packet = { id : packets_type; data : Bytes.t; offset : int }
 
 let empty bytes =
   {
-    id = Convert.packet_type_from_id (Bytes.get_uint16_be bytes 0);
+    id = Convert.packet_type_from_id (Bytes.get_uint8 bytes 0);
     data = bytes;
-    offset = 2;
+    offset = 1;
   }
 
 let read_uint8 packet =
