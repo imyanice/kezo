5bcf672e06f8ae62c6118aa4bf4a7e507aa1dd7e
Yanice <git@yanice.me>
2026-04-18 18:42:14 +0200

    actual tests

--- client-test/index.ts
+++ client-test/index.ts
@@ -1,31 +1,72 @@
-const socket = await Bun.connect({
-	hostname: 'localhost',
-	port: 8080,
+const HOST = "127.0.0.1";
+const PORT = 8080;
 
-	socket: {
-		data(socket, data) {},
-		open(socket) {},
-		close(socket, error) {},
-		drain(socket) {},
-		error(socket, error) {},
-
-		// client-specific handlers
-		connectError(socket, error) {}, // connection failed
-		end(socket) {}, // connection closed by server
-		timeout(socket) {}, // connection timed out
-	},
-})
-
-const msg = 'hello wold'
-const body = new TextEncoder().encode(msg)
-
-if (body.length > 255) {
-	throw new Error('Message too long for 1-byte length prefix')
+function buildLoginPacket(username: string, _skin: number /* will implemtn! */): Uint8Array {
+  const textEncoder = new TextEncoder();
+  const data = Uint8Array.from([
+    0x00,
+    0x00,
+    0x01,
+    ...textEncoder.encode(username.slice(0, 16)),
+    ...[
+      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+      0x00,
+    ],
+  ]);
+  const [hi, low] = [(data.length >> 8) & 0xff, data.length & 0xff];
+  console.log(hi.toString(16), low.toString(16));
+  data[0] = hi;
+  data[1] = low;
+  console.log(data);
+  return data;
 }
 
-const packet = new Uint8Array(1 + body.length)
-packet[0] = body.length // 1-byte size prefix
-packet.set(body, 1) // payload after size
+function buildLocationPacket(isStreetFighter: boolean, mapId: number): Uint8Array {
+  const payload = new Uint8Array(1);
+  const modeBit = isStreetFighter ? 1 : 0;
+  payload[0] = (modeBit << 7) | (mapId & 0x7f);
+  const data = new Uint8Array([0x00, 0x00, 0x02, ...payload]);
+  const [hi, low] = [(data.length >> 8) & 0xff, data.length & 0xff];
+  console.log(hi.toString(16), low.toString(16));
+  data[0] = hi;
+  data[1] = low;
+  console.log(data);
+  return data;
+}
 
-socket.write(packet)
-//socket.write("!hello")
+function buildErrorTestPacket(): Uint8Array {
+  return new Uint8Array([0xba, 0xad, 0xf0, 0x0d]);
+}
+
+// Start Bun TCP Client
+const socket = await Bun.connect({
+  hostname: HOST,
+  port: PORT,
+  socket: {
+    data(socket, data) {
+      console.log(data);
+    },
+    open(socket) {
+      console.log("client connected");
+
+      console.log("sending login (0x01) packet");
+      socket.write(buildLoginPacket("__XximyanicexX__", 0x00)); // harmful >3
+      return;
+      setTimeout(() => {
+        console.log("sending location (0x02) packet (sf map 5)");
+        socket.write(buildLocationPacket(true, 5));
+      }, 1000);
+
+      setTimeout(() => {
+        console.log("sending bad packet");
+        socket.write(buildErrorTestPacket());
+      }, 2000);
+    },
+    close() {
+      console.log("closed 💔");
+    },
+    error(socket, error) {
+      console.error("nuh uh:", error);
+    },
+  },
+});
